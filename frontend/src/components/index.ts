d86a7ed7f028b356a1648e1697a605fb5107748f
LegwanXDL <legwan_xdl@wp.pl>
2020-07-22 19:05:24 +0200

    fixes

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as JSONTree } from './JSONTree';
+export { default as Paper } from './Paper';
+export { default as Rectangle } from './Rectangle';
+export { default as Sidebar } from './Sidebar';
